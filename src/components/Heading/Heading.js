b63f5987403baa9693a081215b7b00a0ba90091e
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-12 14:24:11 -0500

    ADD heading section and add images

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./Heading.css";
 
 const Heading = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section
+      style={{
+        backgroundImage: `url("https://wallpaperforu.com/wp-content/uploads/2020/07/dark-wallpaper-20072813302745.jpg")`,
+        backgroundSize: `cover`,
+      }}
+      className="heading"
+    >
+      <div className="heading-main">
+        <h1 className="heading_title">CYBERPUNK 2077 REMASTER</h1>
+        <p className="heading_paragraph">
+          Cyberpunk 2077 us ab actuib-adventure RPG set in the metropolis of
+          Knight City. where power, laxury and body modificationa one viluted
+          above all else.
+        </p>
+        <div className="heading-buy">
+          <button className="heading_btn">Buy Now</button>
+          <span className="heading_price">19.99$</span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Heading;
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Home.css'
 
+import Heading from '../../components/Heading/Heading'
+
 const Home = () => {
     return (
         <div className="home">
-
+            <div>
+             <Heading />
+            </div>
         </div>
     );
 };
