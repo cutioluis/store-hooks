bcd226c9aa44d6a8a6aa36ec9855e61f86a1afac
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-12 13:42:29 -0500

    ADD footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+            </ul>
+            <ul>
+                <li>Privacy</li>
+                <li>Rembolso</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Home.css'
 
 import Header from '../../components/Home/Header/Header'
-import Nav from '../../components/Home/Nav/Nav'
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+
 
 
 const Home = () => {
@@ -10,6 +12,7 @@ const Home = () => {
         <div className>
             <Nav />
             <Header />
+            <Footer />
         </div>
     );
 };
