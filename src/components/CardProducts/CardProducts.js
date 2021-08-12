1271a295688a5fab1f37e90e5edfdcfb58470642
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-12 15:13:18 -0500

    ADD constants and map array

--- src/components/CardProducts/CardProducts.js
+++ src/components/CardProducts/CardProducts.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./CardProducts.css";
 
+import { products } from "../../constants/constants";
+
 const CardProducts = () => {
   return (
     <section className="products">
-      <div className="products_container">
-        <div className="products_container-text">
-          <h2>WOW: Shadowlands</h2>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur,
-            repudiandae soluta unde architecto ipsum reiciendis.
-          </p>
-        </div>
-        <div className="products_container-buy">
-            <span className="heading_price">21.99$</span>
-            <button className="heading_btn">Add to Card</button>
-        </div>
-      </div>
+      {
+        products.map(product => (
+            <div className="products_container">
+            <div className="products_container-text">
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+            </div>
+            <div className="products_container-buy">
+              <span className="heading_price">${product.price}$</span>
+              <button className="heading_btn">Add to Card</button>
+            </div>
+          </div>
+        ))
+      }
     </section>
   );
 };
---
+++ src/constants/constants.js
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    title: "WOW: Shadowlands",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, repudiandae soluta unde architecto ipsum reiciendis.",
+    image: "/img/luis-home2.svg",
+    id: 1,
+    price: 24.99
+  },
+  {
+    title: "Terraria",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, repudiandae soluta unde architecto ipsum reiciendis.",
+    image: "/img/luis-home3.svg",
+    id: 2,
+    price: 80.99
+
+  },
+  {
+    title: "CyberPunk Remaster",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, repudiandae soluta unde architecto ipsum reiciendis.",
+    image: "/img/luis-home4.svg",
+    id: 3,
+    price: 50.99
+  },
+];
